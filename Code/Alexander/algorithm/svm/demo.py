01c5fbf1e44d34957e4243d673481dd70a78c785
Will <2623556566@qq.com>
2019-12-24 23:42:13 +0800

    add

---
+++ Code/Alexander/algorithm/svm/demo.py
@@ -0,0 +1,28 @@
+from sklearn.svm import SVR
+from sklearn.externals import joblib
+from sklearn.model_selection import GridSearchCV
+import numpy as np
+import matplotlib.pyplot as plt
+
+rng = np.random
+# svr = joblib.load('svr.pkl')        # 读取模型
+
+x = rng.uniform(1, 100, (100, 1))
+y = 5 * x + np.sin(x) * 5000 + 2 + np.square(x) + rng.rand(100, 1) * 5000
+
+# 自动选择合适的参数
+svr = GridSearchCV(SVR(), param_grid={"kernel": ("linear", 'rbf'), "C": np.logspace(-3, 3, 7), "gamma": np.logspace(-3, 3, 7)})
+svr.fit(x, y.ravel())
+# joblib.dump(svr, 'svr.pkl')        # 保存模型
+
+xneed = np.linspace(0, 100, 100)[:, None]
+y_pre = svr.predict(xneed)# 对结果进行可视化：
+plt.scatter(x, y, c='k', label='data', zorder=1)
+# plt.hold(True)
+plt.plot(xneed, y_pre, c='r', label='SVR_fit')
+plt.xlabel('data')
+plt.ylabel('target')
+plt.title('SVR versus Kernel Ridge')
+plt.legend()
+plt.show()
+print(svr.best_params_)
--- Code/Alexander/algorithm/svm/svr.py
+++ Code/Alexander/algorithm/svm/svr.py
@@ -3,13 +3,15 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVR
+from sklearn.svm import SVR
 
 
 def linear_svr(X, y):
     svm_reg = Pipeline([("scaler", StandardScaler()),
+                        # “街道”的宽度由超参数ϵ控制
                         ("linear_svc", LinearSVR(epsilon=1.5))])
     svm_reg.fit(X, y.ravel())
-    print(svm_reg.predict([[5.5, 1.7]]))
+    print(svm_reg.predict(X))
 
 
 if __name__ == "__main__":
@@ -18,3 +20,6 @@ if __name__ == "__main__":
     y = 4 + 3 * X + np.random.randn(1000, 1)
 
     linear_svr(X, y)
+
+    svm_poly_reg = SVR(kernel="poly", degree=2, C=100, epsilon=0.1)
+    svm_poly_reg.fit(X, y)
