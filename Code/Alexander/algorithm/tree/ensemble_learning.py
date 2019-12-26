be43eeed9287d2afafc68e251c5b6631f3627c68
Will <2623556566@qq.com>
2019-12-26 23:34:40 +0800

    add

---
+++ Code/Alexander/algorithm/tree/ensemble_learning.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+from sklearn.metrics import accuracy_score
+
+
+# 软投票和硬投票
+def hard_and_sort_vote(X_train, X_test, y_train, y_test):
+    log_clf = LogisticRegression()
+    rnd_clf = RandomForestClassifier()
+    svm_clf = SVC(probability=True)
+    voting_clf = VotingClassifier(estimators=[('lr', log_clf), ('rf', rnd_clf), ('svc', svm_clf)], voting='soft')
+    # voting_clf = VotingClassifier(estimators=[('lr', log_clf), ('rf', rnd_clf), ('svc', svm_clf)], voting='hard')
+    voting_clf.fit(X_train, y_train)
+
+    for clf in (log_clf, rnd_clf, svm_clf, voting_clf):
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        print(clf.__class__.__name__, accuracy_score(y_test, y_pred))
+
+
+if __name__ == "__main__":
+    # 生成月亮数据集
+    X, y = make_moons(n_samples=1000, noise=0.3)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    hard_and_sort_vote(X_train, X_test, y_train, y_test)
+
